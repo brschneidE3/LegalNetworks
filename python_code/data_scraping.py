6335b836acfd328e420f995426efa882eb64da32
michaelkkim <keehankim@gmail.com>
2016-07-10 01:02:10 -0400

    much python code changed by michael 7/10/16

--- python_code/data_scraping.py
+++ python_code/data_scraping.py
@@ -2,6 +2,23 @@
 """
 ADD A DESCRIPTION OF WHAT THIS FILE IS FOR
 """
+
+"""
+##################### NOTES TO BRENDAN/IAIN: #####################
+
+What I (michaelkkim) try to fix (assuming they were problems, which I'm not sure yet):
+1. fixed weird key values i.e. lines 42, 44, 46 in the original code "data_scraping.py"
+    the weird key values, respectively:
+        'cluster', 'dockets', 'court'
+    you can check them out in this url, but they can't be found: https://www.courtlistener.com/api/rest/v3/clusters/
+
+2. I fixed the line with key value "citation_id" as well (more explanation below)
+3. I did the directory string syntax as same as mcintosh/linux command/bash
+    Note: slash for mac/linux
+          backslash for windows cmd
+
+"""
+
 __author__ = 'brsch, michaelkkim'
 
 import helper_functions
@@ -9,9 +26,10 @@ import os
 
 import urllib
 import json
+import re
 
 proj_cwd = os.path.dirname(os.getcwd())
-data_dir = proj_cwd + r'\data'
+data_dir = proj_cwd + r'/data'
 
 
 def pull_cases_by_court(court_id):
@@ -31,7 +49,7 @@ def pull_cases_by_court(court_id):
     case_numbers_and_parent_directories = []
 
     # Make sure we already have these subdirectories. Create them if not.
-    subdirectories = [data_dir + r'\clusters\%s' % court_id, data_dir + r'\clusters\%s' % court_id]
+    subdirectories = [data_dir + r'/clusters/%s' % court_id, data_dir + r'/clusters/%s' % court_id] ## im not sure why this list has 2 elements of the same kind...
     for subdir in subdirectories:
         if helper_functions.subdir_exists(subdir) is False:
             helper_functions.create_subdir(subdir)
@@ -42,14 +60,19 @@ def pull_cases_by_court(court_id):
         # Iterate through each cluster on current page
         for individual_cluster in clusters_dict['results']:
 
-            new_cluster_url = individual_cluster['cluster']
+            new_cluster_url = individual_cluster['resource_uri']
             new_cluster_dict = helper_functions.url_to_dict(new_cluster_url)
-            new_cluster_docket_url = new_cluster_dict['dockets']
+            new_cluster_docket_url = new_cluster_dict['docket']
             new_cluster_docket_dict = helper_functions.url_to_dict(new_cluster_docket_url)
-            new_cluster_court = new_cluster_docket_dict['court']
+            new_cluster_court_url = new_cluster_docket_dict['court']
+            match_court = re.search(r'(/courts/)(\w+)',  new_cluster_court_url)
+            new_cluster_court = match_court.group(2)
 
             if new_cluster_court == court_id:
-                file_number = new_cluster_dict['citation_id']
+                # file_number = new_cluster_dict['citation_id']
+                # NOTE: with the way we attributed the file_number in the GitHub json files, I think it's more correct to get it from the url, not the key, 'citation_id'
+                match_file_number = re.search(r'(/clusters/)(\d+)',new_cluster_url)
+                file_number = match_file_number.group(2)
                 parent_directory = court_id
                 case_numbers_and_parent_directories.append([file_number, parent_directory])
 
@@ -61,14 +84,19 @@ def pull_cases_by_court(court_id):
     # Iterate through each cluster on last page
     for individual_cluster in clusters_dict['results']:
 
-        new_cluster_url = individual_cluster['cluster'] ## where is this key?
+        new_cluster_url = individual_cluster['resource_uri']
         new_cluster_dict = helper_functions.url_to_dict(new_cluster_url)
-        new_cluster_docket_url = new_cluster_dict['dockets'] ## where is this key?
+        new_cluster_docket_url = new_cluster_dict['docket']
         new_cluster_docket_dict = helper_functions.url_to_dict(new_cluster_docket_url)
-        new_cluster_court = new_cluster_docket_dict['court'] ## where is this key?
+        new_cluster_court_url = new_cluster_docket_dict['court']
+        match_court = re.search(r'(/courts/)(\w+)',  new_cluster_court_url)
+        new_cluster_court = match_court.group(2)
 
         if new_cluster_court == court_id:
-            file_number = new_cluster_dict['citation_id']
+            # file_number = new_cluster_dict['citation_id']
+            # NOTE: with the way we attributed the file_number in the GitHub json files, I think it's more correct to get it from the url, not the key, 'citation_id'
+            match_file_number = re.search(r'(/clusters/)(\d+)',new_cluster_url)
+            file_number = match_file_number.group(2)
             parent_directory = court_id
             case_numbers_and_parent_directories.append([file_number, parent_directory])
 
@@ -80,9 +108,9 @@ def is_downloaded(file_number, parent_directory):
     # TODO
     """
     Should check if corresponding cluster & opinion json files have been downloaded to appropriate path:
-        data_dir + r'\clusters\COURT NAME\FILE NUMBER.json'
+        data_dir + r'/clusters/COURT NAME/FILE NUMBER.json'
                             &
-        data_dir + r'\opinions\COURT NAME\FILE NUMBER.json'
+        data_dir + r'/opinions/COURT NAME/FILE NUMBER.json'
 
     Return True or False.
     """
@@ -104,13 +132,16 @@ def download_case(new_cluster_url):
 
     ## get the court_id / parent_directory
     new_cluster_dict = helper_functions.url_to_dict(new_cluster_url)
-    new_cluster_docket_url = new_cluster_dict['dockets']
+    new_cluster_docket_url = new_cluster_dict['docket']
     new_cluster_docket_dict = helper_functions.url_to_dict(new_cluster_docket_url)
-    new_cluster_court = new_cluster_docket_dict['court']
+    new_cluster_court_url = new_cluster_docket_dict['court']
+    match_court = re.search(r'(/courts/)(\w+)',  new_cluster_court_url)
+    new_cluster_court = match_court.group(2)
     parent_directory = new_cluster_court
 
     ## get the citation_id / file_number
-    file_number = new_cluster_dict['citation_id']
+    match_file_number = re.search(r'(/clusters/)(\d+)',new_cluster_url)
+    file_number = match_file_number.group(2)
 
     ## make cluster.json into a file with the proper directory path
     file_name = data_dir + r'/clusters/' + parent_directory + r'/' + file_number + r'.json'
@@ -128,10 +159,6 @@ def download_case(new_cluster_url):
     with open(file_name2, 'w') as outfile2:
         json.dumps(js2, outfile2, indent=4)
 
-
-    ## TODO? :
-    ## I just follow the same code syntax as lines 45, 47, 49 for the unknown keys... so this code isn't foolproof yet logically speaking
-    ## im still not sure where the keys are found "cluster", "dockets", "court" in lines 45, 47, 49 (any tips Brendan?)
     pass
 
 
